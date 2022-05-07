a922e0e7836d4f977b21a849c0706625948295b5
wanghao1 <wanghao1@sg.cambricon.com>
2022-05-07 15:57:07 +0800

    remove unrelated

--- news/include/iou3d_nms.h
+++
@@ -1,6 +0,0 @@
-#include <vector>
-#include <cuda.h>
-#include <cuda_runtime_api.h>
-
-int nms_gpu(const float* boxes_data, long* keep_data, int boxs_num,  float nms_overlap_thresh);
-int raw_nms_gpu(const float* reg,  const float* height, const float* dim , const float* rot, const int* indexs, long* keep_data, int boxes_num,  float nms_overlap_thresh);
